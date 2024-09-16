5e2cc17bb714e604cab398a014e9936ebbb8fbee
mdmeeuwisse <michiel.meeuwisse@gmail.com>
2024-09-16 12:03:13 +0200

    section 2 part 3 werkt niet helemaal maarja

--- src/main/java/section2_syntax/part3_flow_control/AllSubstringsPrinter.java
+++ src/main/java/section2_syntax/part3_flow_control/AllSubstringsPrinter.java
@@ -12,6 +12,7 @@ package section2_syntax.part3_flow_control;
 public class AllSubstringsPrinter {
     /**
      * main method serves development purposes only.
+     *
      * @param args the args
      */
     public static void main(String[] args) {
@@ -22,52 +23,59 @@ public class AllSubstringsPrinter {
     /**
      * Given a String object, it should print all possible substrings of it where
      * in each iteration the rightmost character is left out.
-     * @param stringToSubstring the string to substring
+     *
+     * @param word the string to substring
      */
-    public void printSubstringsLeftAlignedLeftTruncated(String stringToSubstring) {
-        //YOUR CODE
+    public void printSubstringsLeftAlignedLeftTruncated(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            String subString = word.substring(i, word.length());
+            System.out.println(subString);
+        }
     }
 
     /**
      * Prints all possible substrings according to arguments. You can make use of {@link #createSpacer(int length) createSpacer} method.
-     * @param stringToSubstring the string to substring
-     * @param leftTruncated flag to indicate whether the substrings should be truncated from the left (or the right)
-     * @param leftAligned flag to indicate whether the substrings should be printed left-aligned (or right-aligned)
-     */
-    public void printAllSubstrings(String stringToSubstring, boolean leftTruncated, boolean leftAligned) {
-        //YOUR CODE
-    }
-
-    /**
-     * Creates a spacer of given length. This solution uses a StringBuilder which is the correct way to concatenate
-     * Strings in Java.
      *
-     * @param length the spacer length, in number of spaces
-     * @return spacer
+     * @param word          the string to substring
+     * @param leftTruncated flag to indicate whether the substrings should be truncated from the left (or the right)
+     * @param leftAligned   flag to indicate whether the substrings should be printed left-aligned (or right-aligned)
      */
-    public String createSpacer(int length) {
-//        String spacer = "";
-        StringBuilder sb = new StringBuilder("");
-        for (int i = 0; i < length; i++) {
-            sb.append(" ");
-//            spacer += " ";
-//            spacer = spacer + " ";
+    public void printAllSubstrings(String word, boolean leftTruncated, boolean leftAligned) {
+        if (leftAligned) {
+            if (leftTruncated) {
+                for (int i = 0; i < word.length(); i++) {
+                    String subString = word.substring(i, word.length());
+                    System.out.println(subString);
+                }
+            } else {
+                for (int i = 0; i < word.length(); i++) {
+                    String subString = word.substring(0, word.length() - i);
+                    System.out.println(subString);
+                }
+            }
+        } else{
+            if (leftTruncated) {
+                for (int i = 0; i < word.length(); i++) {
+                    String subString = word.substring(i, word.length());
+                    String spacer = createSpacer(i);
+                    System.out.println(spacer + subString);
+                }
+            } else {
+                for (int i = 0; i < word.length(); i++) {
+                    String subString = word.substring(0, word.length() - i);
+                    System.out.println(subString);
+                    String spacer = createSpacer(i);
+                    System.out.println(spacer + subString);
+                }
+            }
         }
-//        return spacer;
-        return sb.toString();
     }
 
-    /**
-     * THIS IS AN EXAMPLE OF HOW NOT TO DO IT!!
-     * String concatenation in for-loops is *evil*
-     * @param length
-     * @return spacer
-     */
-    private String createSpacerTheWrongWay(int length) {
-        String spacer = "";
+    private String createSpacer(int length) {
+        StringBuilder spacer = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            spacer += " ";
+            spacer.append(' ');
         }
-        return spacer;
+        return spacer.toString();
     }
 }
